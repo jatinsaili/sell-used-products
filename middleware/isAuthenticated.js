ce7bdd43d5b08d68ec31d56450010d938c25a95a
Jatin Saili <jsaili@my.centennialcollege.ca>
2023-08-19 00:01:38 +0530

    updated route

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -4,6 +4,6 @@ module.exports = (req, res, next) => {
     if (req.session && req.session.userId) {
         next(); // User is authenticated, proceed to the next middleware or route handler
     } else {
-        res.redirect('/login'); // Redirect to login page if not authenticated
+        res.redirect('/user/login'); // Redirect to login page if not authenticated
     }
 };
