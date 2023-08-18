136347f6f06b7a0e92e6eb5728b8c8737fe5e3ce
Jatin Saili <jsaili@my.centennialcollege.ca>
2023-08-18 23:25:46 +0530

    added express-session middleware

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Required modules
 const express = require('express');
 const path = require('path');
+const session = require('express-session');
 const db = require('./firebaseConfig');
 const userRoutes = require('./routes/user');
 const adRoutes = require('./routes/ads');
@@ -11,9 +12,24 @@ const AdController = require('./controllers/adController');
 const app = express();
 const PORT = 3000;
 
+app.use(session({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: true
+}));
+
+
 // Set up EJS as the view engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+// Middleware to set res.locals variables
+app.use((req, res, next) => {
+  if (req.session) {
+      res.locals.userId = req.session.userId;
+  }
+  next();
+});
 
 // main page
 app.get('/', AdController.listAds);
